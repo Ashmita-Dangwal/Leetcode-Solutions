a24620b31d082239ec84b03fffc76ead5d90626a
Ashmita Dangwal <ashmitadangwal43@gmail.com>
2026-08-23 19:53:48 +0530

    Time: 3 ms (90.38%), Space: 83.1 MB (84.56%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    int dp[];
+    public int maxJump(int[] stones) {
+        int ans = stones[1] - stones[0];
+        for(int i = 2; i < stones.length; i++){
+            ans = Math.max(ans, stones[i] - stones[i-2]);
+        }
+        return ans;
+    }
+}
